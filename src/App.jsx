08bac271cde35e1d21209709f4174cd59cab27c8
Sai Krishna Das <saikrishnadas666@gmail.com>
2025-08-20 19:28:23 +0530

    Adding Hero Section and layouts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css"
 import gsap from "gsap"
 import {ScrollTrigger, SplitText} from "gsap/all" 
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 //This will register the plugins for gsap - use only what is needed to keep the bundle size down.
 gsap.registerPlugin(ScrollTrigger,SplitText);
@@ -11,6 +12,8 @@ const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
+      <div className='h-dvh'></div>
     </main>
   )
 }
